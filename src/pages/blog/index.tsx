4fb0b8e7721f1b687d6710d1814be0b3e22c91d6
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-15 15:33:14 -0700

    Create routes

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+/**
+ * Blog will have these components:
+ * 1. blog posts - React, Next, AI/ML, Math
+ * 2. random thoughts 
+ */
---
+++ src/pages/setTheme/index.tsx
@@ -0,0 +1,3 @@
+/**
+ * Route for calling the settheme function
+ */
