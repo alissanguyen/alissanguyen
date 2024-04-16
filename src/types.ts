bd3372f48fc7b46dc6cfcdd54c1a3b695f4859e5
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-15 23:55:53 -0700

    Define basic types, install new dependencies, organize files for different components

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+// TypeScript types definition
+
+export enum SupportedTheme {
+  DARK = "dark",
+  LIGHT = "light"
+}
+
+export type blogPostCategories = {
+  id: string,
+  category: string
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class", // Enable the dark mode based on the 'class' attribute
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+
+        // Define your light mode color palette
+        light: {
+          bgPrimary: '#4A90E2',
+          bgSecondary: '#171717',
+          text: 'black',
+          textSecondary: '#cdcdcf'
+        },
+
+        // Define your dark mode color palette
+        dark: {
+          bgPrimary: '#101010',
+          bgSecondary: '#171717',
+          textPrimary: '#737278', //dimmed gray
+          textSecondary: '#cdcdcf' //lighter gray, used for hover on main page
+        },
+
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
