60a91c5025056f6cd7528549968c9028a9bc2aa7
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-16 16:40:30 -0700

    90% finish on home section

---
+++ src/components/HomeSection/HomeSection.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
+import * as React from 'react';
+import CircleWithRotatingText from '../ScrollForMoreButton/ScrollForMoreButton';
+import "./HomeSection.css";
+import SocialHandles from '../SocialHandles/SocialHandles';
+import MyWorkButton from '../MyWorkButton/MyWorkButton';
+import ResumeButton from '../ResumeButton/ResumeButton';
+
+interface Props {
+
+}
+
+const HomeSection: React.FC<Props> = ({ }) => {
+    return (
+        <div className='flex flex-row items-end h-full justify-between'>
+            <div className='flex flex-col items-start h-full justify-center gap-14 pt-sans pl-20'>
+                <p className='text-xl caption-regular'>Chronic Software Programmer, Web Designer, Cat Petter</p>
+
+                <div className='caption-bold text-9xl flex flex-col items-start gradient-text '>
+                    <span>I'm Alissa Nguyen</span>
+                    <span>Web Developer and </span>
+                    <span>Software Engineer.</span>
+                </div>
+                <div className='flex flex-row w-full justify-between'>
+                    <div className='HomeSection__Buttons flex flex-row gap-16'>
+                        <MyWorkButton />
+                        <ResumeButton />
+                    </div>
+                </div>
+            </div>
+
+            <CircleWithRotatingText />
+        </div>
+    )
+}
+
+export default HomeSection
