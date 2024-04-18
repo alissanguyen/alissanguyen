b41e3baa500114eae619f171eb643b43997128df
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-17 22:30:04 -0700

    Add Portfolio section component

--- src/components/PortfolioSection/PortfolioSection.tsx
+++ src/components/PortfolioSection/PortfolioSection.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import SparkleSVG from '../SparkleSVG';
-import SkillsSection from '../SkillsSection/SkillsSection';
 import ChessImage from "../../asset/chess.png";
 import StockImage from "../../asset/stock.png";
 import ImgGeneratorImage from "../../asset/image.png";
 import SignLanguageImage from "../../asset/sign.png"
 import ProjectCard from './ProjectCard';
 import "./PortfolioSection.css";
-import OtherSkillsSection from '../OtherSkillsSection/OtherSkillsSection';
 
 interface Props {
 
@@ -41,15 +39,13 @@ const projects = [
 ]
 const PortfolioSection: React.FC<Props> = ({ }) => {
     return (
-        <div className='flex flex-col items-start justify-center gap-14 pl-20 ' id="portfolio">
-            <div className='flex flex-row items-center text-2xl gap-3 font-semibold uppercase'>
+        <div className='flex flex-col items-start justify-center gap-14 pl-20' id="portfolio">
+            <div className='flex flex-row items-center text-2xl gap-3 font-medium rounded-3xl border-2 border-dark-textPrimary px-8 py-2'>
                 <SparkleSVG />
                 <p>Portfolio</p>
             </div>
             <ul className="FeaturedProjects__Wrapper grid grid-cols-3 gap-10">{projects.map((project) => <ProjectCard key={project.id} project={project} />)}</ul>
 
-            <SkillsSection />
-            <OtherSkillsSection />
         </div>
     )
 }
