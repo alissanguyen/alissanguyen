b31c3cf5c02756d665d08d0a01cc4f5458aeba28
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-18 22:10:20 -0700

    Add skeleton React component for pages/setTheme and pages/blog/[slug] to fix Vercel deployment error

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface Props {
+
+}
+
+const BlogPostPage: React.FC<Props> = ({ }) => {
+ return (
+ <div>Individual blog post page</div>
+ )
+}
+
+export default BlogPostPage
--- src/pages/setTheme/index.tsx
+++ src/pages/setTheme/index.tsx
@@ -1,3 +1,17 @@
 /**
  * Route for calling the settheme function
  */
+
+import * as React from 'react';
+
+interface Props {
+
+}
+
+const SetThemePage: React.FC<Props> = ({ }) => {
+    return (
+        <div>Route for calling setTheme</div>
+    )
+}
+
+export default SetThemePage
